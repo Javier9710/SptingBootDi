92c107e51835fb3ce854cb21e224806ecf457548
unknown <javierandresmd@ufps.edu.co>
2020-12-30 19:25:22 -0500

    inyeccion de dependencias con @Autowired

---
+++ src/main/java/com/ufps/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,13 @@
+package com.ufps.springboot.di.app.models.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MiServicio {
+	
+	public String operacion() {
+		
+		return "ejecutando algun proceso importante......";
+	}
+
+}
